c498bb0f2297188a2aa2ffac0705309419cd0f61
David Stapleton <david@platypi.io>
2016-03-29 11:44:30 -0500

    Added friend removal, and a button to restore all friends

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -9,25 +9,41 @@ export default class HomeViewControl extends BaseViewControl {
             {
                 name: 'Tyler',
                 age: 26,
-                title: 'Software Developer'
+                title: 'Software Developer',
+                show: true
             },
             {
                 name: 'Hillary',
                 age: 26,
-                title: 'Software Developer'
+                title: 'Software Developer',
+                show: true
             },
             {
                 name: 'Haley',
                 age: 26,
-                title: 'Depot/U Principal'
+                title: 'Depot/U Principal',
+                show: true
             },
             {
                 name: 'Matt',
                 age: 38,
-                title: 'Depot/U CEO'
+                title: 'Depot/U CEO',
+                show: true
             }
         ]
     };
+    
+    hideFriend(friendIndex: number): void {
+        let friendToHide = this.context.friends[friendIndex];
+        friendToHide.show = false;
+    }
+    
+    restoreFriends(): void {
+        for (let i = 0; i < this.context.friends.length; i++) {
+            let friend = this.context.friends[i];
+            friend.show = true;
+        }
+    }
 }
 
 register.viewControl('home-vc', HomeViewControl);
